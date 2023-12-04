e93f6ffec95606b96051492a427bf59bbbb10f78
Cyuen0310 <chanszeyuen0310@icloud.com>
2023-12-05 07:16:05 +0800

    main

--- navbar.php
+++ navbar.php
@@ -8,7 +8,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/globle-style.css" />
+    <link rel="stylesheet" href="../css/globle-style.css" />
+
 
   <title>Document</title>
 </head>
